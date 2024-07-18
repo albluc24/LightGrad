7cbe39806932d05a27ac53fae87fd9cf87c3f525
albluc24 <albluc24@hotmail.com>
2024-07-18 11:13:19 +0200

    stripped

--- LightGrad/dataset.py
+++ LightGrad/dataset.py
@@ -2,10 +2,9 @@ import torch
 import torch.utils.data
 import json
 import math
-from librosa.filters import mel as librosa_mel_fn
 import re
 import torchaudio
-
+import numpy as np
 from torch.nn.utils.rnn import pad_sequence
 
 
@@ -23,14 +22,7 @@ class Dataset(torch.utils.data.Dataset):
     def __init__(self,
                  datalist_path,
                  phn2id_path,
-                 sample_rate,
-                 n_fft,
-                 n_mels,
-                 fmin,
-                 fmax,
-                 hop_size,
-                 win_size,
-                 add_blank=True):
+                 add_blank=False):
         super().__init__()
         with open(datalist_path) as f:
             self.datalist = json.load(f)
@@ -38,66 +30,28 @@ class Dataset(torch.utils.data.Dataset):
             self.phone_set = json.load(f)
 
         self.add_blank = add_blank
-        self.sample_rate = sample_rate
-        self.n_fft = n_fft
-        self.n_mels = n_mels
-        self.fmin = fmin
-        self.fmax = fmax
-        self.hop_size = hop_size
-        self.win_size = win_size
         self.cache = {}
-        self.hann_window = torch.hann_window(win_size)
-        self.mel_basis = torch.from_numpy(
-            librosa_mel_fn(sr=sample_rate,
-                           n_fft=n_fft,
-                           n_mels=n_mels,
-                           fmin=fmin,
-                           fmax=fmax)).float()
 
     def get_vocab_size(self):
         # PAD is also considered
         return len(self.phone_set) + 1
 
     def load_audio_and_melspectrogram(self, audio_path):
-        audio, original_sr = torchaudio.load(audio_path)
-        if original_sr != self.sample_rate:
-            audio = torchaudio.functional.resample(audio, original_sr,
-                                                   self.sample_rate)
-        audio = torch.nn.functional.pad(audio.unsqueeze(1), (int(
-            (self.n_fft - self.hop_size) /
-            2), int((self.n_fft - self.hop_size) / 2)),
-                                        mode='reflect')
-        audio = audio.squeeze(1)
-        spec = torch.stft(audio,
-                          self.n_fft,
-                          self.hop_size,
-                          self.win_size,
-                          self.hann_window,
-                          False,
-                          onesided=True,
-                          return_complex=True)
-        spec = spec.abs()
-        spec = torch.matmul(self.mel_basis, spec)
-        spec = spectral_normalize_torch(spec).squeeze(0)
-        # audio: (1,T) spec: (T,n_mels)
-        return audio, spec.T
+        emb=torch.tensor(np.load(audio_path))
+        return emb
 
     def load_item(self, i):
         #item_name, wav_path, text, phonemes = self.datalist[i]
         item_name = self.datalist[i]['name']
-        wav_path = self.datalist[i]['wav_path']
-        text = self.datalist[i]['text']
+        wav_path = self.datalist[i]['emb_path']
         phonemes = self.datalist[i]['phonemes']
-
-        audio, mel = self.load_audio_and_melspectrogram(wav_path)
+        mel=self.load_audio_and_melspectrogram(wav_path)
         if self.add_blank:
             phonemes = " <blank> ".join(phonemes).split(' ')
         phonemes = ['<bos>'] + phonemes + ['<eos>']
         ph_idx = [self.phone_set[x] for x in phonemes if x in self.phone_set]
         self.cache[i] = {
             'item_name': item_name,
-            'txt': text,
-            'wav': audio,
             'ph': phonemes,
             'mel': mel,
             'ph_idx': ph_idx
@@ -105,24 +59,11 @@ class Dataset(torch.utils.data.Dataset):
         return self.cache[i]
 
     def __getitem__(self, i):
-        return self.cache.get(i, self.load_item(i))
+        if i not in self.cache: return self.load_item(i)
+        else: return self.cache[i]
 
     def process_item(self, item):
-        ph = item['ph']
-        # remove original | because this indicates word boundary
-        ph = re.sub(r' \|', '', ph).split(' ')
-        if self.add_blank:
-            # add | as the phoneme boundary
-            ph = ' | '.join(ph).split(' ')
-        new_item = {
-            'item_name': item['item_name'],
-            'txt': item['txt'],
-            'ph': ph,
-            'mel': item['mel'],
-            'ph_idx': [self.phone_set[x] for x in ph if x in self.phone_set],
-            'wav': item['wav'],
-        }
-        return new_item
+        return item
 
     def __len__(self):
         return len(self.datalist)
